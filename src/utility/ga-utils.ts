c19377204d137c3d9e4157680d2db3c2eb402931
Ansgar Hoyer <ansg.hoy@gmail.com>
2021-03-20 23:22:08 +0100

    tried some stuff

--- src/utility/ga-utils.ts
+++ src/utility/ga-utils.ts
@@ -2,6 +2,8 @@ import * as ReactGA from "react-ga";
 
 export const initGA = (id: string) => {
   if (process.env.NODE_ENV === "production") {
+    console.log("initialized GA!")
     ReactGA.initialize(id);
+    ReactGA.pageview(window.location.pathname + window.location.search);
   }
 };
